1dfbc323f19061993402dcfd9ce25b0dc563fa0a
githubschman <sarahhubschmail@gmail.com>
2017-07-28 16:07:12 -0400

    context menu added

--- js/background.js
+++ js/background.js
@@ -54,10 +54,47 @@ chrome.browserAction.onClicked.addListener(function (tab) {
       console.assert(typeof url == 'string', 'tab.url should be a string');
       if(url){
         siteInfo.currentUrl = url;
-        // alert('visited' + siteInfo.currentUrl)
         saveChanges(url);
       }
     })
   }
     
 });
+
+
+//  The Context menu will be active on the page in general, on any text selection, 
+// image (or element that has a “src” attribute) and all links.
+
+
+
+
+ var clickHandler = function(e) {
+    // var url = e.pageUrl;
+    // var buzzPostUrl = "http://www.google.com/buzz/post?";
+
+    // if (e.selectionText) {
+    //     // The user selected some text, put this in the message.
+    //     buzzPostUrl += "message=" + encodeURI(e.selectionText) + "&";
+    // }
+
+    // if (e.mediaType === "image") {
+    //     buzzPostUrl += "imageurl=" + encodeURI(e.srcUrl) + "&";
+    // }
+
+    // if (e.linkUrl) {
+    //     // The user wants to buzz a link.
+    //     url = e.linkUrl;
+    // }
+
+    // buzzPostUrl += "url=" + encodeURI(url);
+
+    // // Open the page up.
+    // chrome.tabs.create(
+    //       {"url" : buzzPostUrl });
+};
+
+chrome.contextMenus.create({
+    "title": "Save Your Place",
+    "contexts": ["page", "selection", "image", "link"],
+    "onclick" : clickHandler
+  });
