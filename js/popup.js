bf20bf923cceaf0150c1c86445d1de53f279141c
githubschman <sarahhubschmail@gmail.com>
2017-08-03 15:39:17 -0400

    event listener actually working!

--- js/popup.js
+++ js/popup.js
@@ -14,10 +14,10 @@ chrome.tabs.query(queryInfo, function(tabs) {
 
     if(url){
         chrome.storage.sync.get([url], function(result) {   
-            console.log(url)
+            // console.log(url)
             if(result[url]){
                 result[url].forEach(note => {
-                    $("#points ul").append('<li>' + note + ' <button class="delete" id="' + note + '"> DEL </button>' + '</li>');
+                    $("#points ul").append('<li>' + note + '<button>edit</button> <button class="delete" id="' + note + '"> DEL </button>' + '</li>');
                 })
             }
         });
@@ -25,18 +25,52 @@ chrome.tabs.query(queryInfo, function(tabs) {
 })  
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    let button = document.getElementsByClassName('delete');
-    // onClick's logic below:
-    console.log(button)
-    button.addEventListener('click', function() {
-        console.log('tryna delete stuff')
+// pop out your notes??
+function showDialog(){
+    chrome.windows.create({
+        url: 'dialog.html',
+        width: 200,
+        height: 120,
+        type: 'popup'
     });
+}    
+
+function init() {
+    dialog = document.querySelector('#points');
+    dialog.addEventListener('click', showDialog, false);
+}    
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let el = document.getElementsByClassName("delete");
+    // HTMLCollection(1)
+    console.log(el)
+    if(el.length){
+        var arr = Array.prototype.slice.call( el )
+        console.log(arr)
+    }
+
+
+   
+    // for (var i = 0; i < el.length; i++) {
+    //     console.log('anything?')
+    //     console.log(el[i].id); //second console output
+    //     el[i].onClick(function(){
+    //         console.log('clicked button')
+    //     })
+    // }
+
+
+    // onClick's logic below:
+    // el.addEventListener("click", function(){console.log('um ok')}, false);
 });
 
+////////////////////////////////////
 
-// var notifier,
-//     dialog;
+
+
+var notifier,
+    dialog;
 
 // function showNotify() {
 //     var notify;
@@ -52,19 +86,6 @@ document.addEventListener('DOMContentLoaded', function() {
 //         window.webkitNotifications.requestPermission();
 //     }
 // }    
-// function showDialog(){
-//     chrome.windows.create({
-//         url: 'dialog.html',
-//         width: 200,
-//         height: 120,
-//         type: 'popup'
-//     });
-// }    
-// function init() {
-//     clicker = document.querySelector('#click');
-//     dialog = document.querySelector('#dialog');
 
-//     clicker.addEventListener('click', showNotify, false);
-//     dialog.addEventListener('click', showDialog, false);
-// }    
-// document.addEventListener('DOMContentLoaded', init);
+
+document.addEventListener('DOMContentLoaded', init);
